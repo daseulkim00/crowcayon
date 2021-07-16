95d644efec529701ee46fdf5bbf652b4f29a8e81
daseulkim00 <87412953+daseulkim00@users.noreply.github.com>
2021-07-16 10:37:50 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$('.t_right>div').hover(function () {
+    $(this).children('.hover').fadeIn()
+}, function () {
+    $(this).children('.hover').fadeOut()
+})
+$('.sec3>article').hover(function () {
+    $(this).children('.hover').fadeIn()
+}, function () {
+    $(this).children('.hover').fadeOut()
+})
